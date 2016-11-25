ee96c12c9aa8fe4b93057d72ecac8ececd0a6722
Nadir Miralimov <nadir@3dsellers.com>
2016-11-25 15:27:01 +0200

    Create AmazonProductService, and add it to app container.

---
+++ src/Slidely/Service/AmazonProductService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Slidely\Service;
+
+use Slidely\Model\AmazonProductResponse;
+use Slidely\WebScrapper\AmazonFirstProductScrapper;
+
+class AmazonProductService {
+
+    private $amazonSearchEndpoint;
+    private $amazonKeywordParameter;
+
+    public function __construct(string $amazonSearchEndpoint, string $amazonKeywordParameter) {
+        $this->amazonSearchEndpoint = $amazonSearchEndpoint;
+        $this->amazonKeywordParameter = $amazonKeywordParameter;
+    }
+
+    /**
+     * Retrieves first product from Amazon search page by given keyword.
+     *
+     * @param string $keyword
+     *
+     * @return AmazonProductResponse
+     */
+    public function getFirstProduct(string $keyword): AmazonProductResponse {
+        $url = join([$this->amazonSearchEndpoint, '?',
+                     $this->amazonKeywordParameter, '=', $keyword]);
+
+        $page = file_get_contents($url);
+
+        $scrapper = new AmazonFirstProductScrapper($page);
+        $data = $scrapper->getData();
+
+        $product = new AmazonProductResponse($keyword);
+        $product->setName($data['name']);
+        $product->setRating($data['rating']);
+        $product->setFullPrice($data['full_price']);
+        $product->setLastPrice($data['last_price']);
+
+        return $product;
+    }
+}
--- src/app.php
+++ src/app.php
@@ -5,27 +5,37 @@ use \Psr\Http\Message\ResponseInterface as Response;
 use Slim\App;
 
 use Slim\Container;
-use Slidely\WebScrapper\AmazonFirstProductScrapper;
 
 $configuration = [
     'settings' => [
         'displayErrorDetails' => true,
+        'amazon_search_endpoint' => 'https://www.amazon.com/s/',
+        'amazon_search_keyword_parameter' => 'field-keywords'
     ],
 ];
 
 $c = new Container($configuration);
 
 $app = new App($c);
+
+$container = $app->getContainer();
+
+$container["amazonProductService"] = function($c) {
+    return new \Slidely\Service\AmazonProductService($c['settings']['amazon_search_endpoint'],
+                                                     $c['settings']['amazon_search_keyword_parameter']);
+};
+
 $app->get('/api/product-search', function (Request $request, Response $response) {
 
     $params = $request->getQueryParams();
 
     $keyword = $params['keyword'];
 
-    $pageContents = file_get_contents('https://www.amazon.com/s/?field-keywords=' . $keyword);
-    $scrapper = new AmazonFirstProductScrapper($pageContents);
+    /** @var \Slidely\Service\AmazonProductService $amazonProductService */
+    $amazonProductService = $this->amazonProductService;
 
+    $productResponse = $amazonProductService->getFirstProduct($keyword);
 
-    return $response->withJson($scrapper->getData());
+    return $response->withJson($productResponse->jsonSerialize());
 });
 $app->run();
